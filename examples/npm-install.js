c7f456f51f3098f21a986834f870689a33c000bc
Marak <marak.squires@gmail.com>
2015-10-03 03:52:43 -0700

    [minor] Remove debugging statements.  * Update example

--- examples/npm-install.js
+++ examples/npm-install.js
@@ -1,7 +1,8 @@
 var hpm = require('../');
 
-hpm.npm.install(".", {
-  packages: ["faker"]
+hpm.npm.spawn(process.stdout, {
+  where: "/Users/chroot/",
+  packages: ["hook.io-i18n"]
 }, function (err, result) {
   console.log(err, result);
 })
